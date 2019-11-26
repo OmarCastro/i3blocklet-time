d6808177c32c27bc5588c006cd6369825765f0f7
OmarCastro <oomar.castro.360@gmail.com>
2019-11-26 19:12:19 +0000

    feat: Add command line params to offset scripts

--- python_scripts/appendTzAbbrOffsets.py
+++ python_scripts/appendTzAbbrOffsets.py
@@ -6,8 +6,14 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--sort',  help='sort results by offset', action='store_true')
+parser.add_argument('-f','--tz-field',  type=int, help='field with timezone', required=True)
+parser.add_argument('-s','--separator',  help='input field separator', default=None)
+parser.add_argument('-o','--output-separator',  help='output field separator', default=" ")
 args = parser.parse_args()
+if args.tz_field < 1:
+    parser.error("timezone field must be a positive number")
 
+print('{0}'.format(args))
 now = time.time()
 
 def offsets_in_timezone(timezone_name):
@@ -19,15 +25,15 @@ def offsets_in_timezone(timezone_name):
 
 linesDict = {}
 for line in sys.stdin:
-    fields = line.split()
-    offsets = offsets_in_timezone(fields[0])
+    fields = line.split(args.separator)
+    offsets = offsets_in_timezone(fields[args.tz_field - 1])
     fields += offsets
     if args.sort:
         offset = offsets[1]
         linesDict.setdefault(offset, [])
-        linesDict[offset].append(' '.join(fields));
+        linesDict[offset].append(args.output_separator.join(fields));
     else:
-        print(' '.join(fields))
+        print(args.output_separator.join(fields))
     
 
 if args.sort:
